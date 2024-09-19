304d2561d6a80910913cf785d6268f094957f5bb
Yahia Hassan <yahia.hassan152@gmail.com>
2024-09-20 02:06:14 +0300

    initial commit - with nav bar

---
+++ main.js
@@ -0,0 +1,12 @@
+let menuOpenFlage =false;
+function displayToggledMenu(){
+    let element = document.getElementById('toggled-menu');
+    if(!menuOpenFlage){
+        element.style = `display: flex;`;
+        menuOpenFlage = true;
+    }
+    else{
+        element.style = `display: none;`;
+        menuOpenFlage = false;
+    }
+}
